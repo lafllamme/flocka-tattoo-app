cdb16f1d64412c3777621518c76dc8148a5e046e
Lafllamme <teke900@gmail.com>
2026-08-23 19:36:33 +0200

    feat: add favicon assets and web manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,13 @@ export default defineNuxtConfig({
     head: {
       titleTemplate: '%s',
       meta: [{ name: 'theme-color', content: '#090909' }],
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
     },
   },
 })
